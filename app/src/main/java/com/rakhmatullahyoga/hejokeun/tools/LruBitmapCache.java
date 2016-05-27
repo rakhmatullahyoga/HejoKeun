a12331b37b72594b1c6a602113512b7382d55fc6
Rakhmatullah Yoga Sutrisna <rakhmatullahyoga@gmail.com>
2016-05-27 08:40:09 +0700

    first commit

---
+++ app/src/main/java/com/rakhmatullahyoga/hejokeun/tools/LruBitmapCache.java
@@ -0,0 +1,44 @@
+package com.rakhmatullahyoga.hejokeun.tools;
+
+import android.graphics.Bitmap;
+import android.support.v4.util.LruCache;
+
+import com.android.volley.toolbox.ImageLoader.ImageCache;
+
+/**
+ * Created by Rakhmatullah Yoga S on 27/05/2016.
+ */
+public class LruBitmapCache extends LruCache<String, Bitmap> implements ImageCache {
+    public static int getDefaultLruCacheSize() {
+        final int maxMemory = (int) (Runtime.getRuntime().maxMemory() / 1024);
+        final int cacheSize = maxMemory / 8;
+
+        return cacheSize;
+    }
+    public LruBitmapCache() {
+        this(getDefaultLruCacheSize());
+    }
+    /**
+     * @param maxSize for caches that do not override {@link #sizeOf}, this is
+     *                the maximum number of entries in the cache. For all other caches,
+     *                this is the maximum sum of the sizes of the entries in this cache.
+     */
+    public LruBitmapCache(int maxSize) {
+        super(maxSize);
+    }
+
+    @Override
+    protected int sizeOf(String key, Bitmap value) {
+        return value.getRowBytes() * value.getHeight() / 1024;
+    }
+
+    @Override
+    public Bitmap getBitmap(String url) {
+        return get(url);
+    }
+
+    @Override
+    public void putBitmap(String url, Bitmap bitmap) {
+        put(url, bitmap);
+    }
+}
